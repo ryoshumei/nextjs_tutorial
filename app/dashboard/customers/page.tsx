167c4c1886e8c0a204586d625a8626e079b6b681
daydreamer <s419922572@gmail.com>
2024-04-24 11:30:16 +0900

    Add invoice creation, update, and delete functionality
    Added components and necessary logic for creating, updating, and deleting invoices. Also made some improvements to the Search component and added an error handling component. Updated backend logic to handle changes and validate form inputs.

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,17 @@
+'use client';
+// import { useEffect, useState } from 'react';
+
 export default function Page() {
+    // const [loading, setLoading] = useState(true);
+    //
+    // useEffect(() => {
+    //     const timer = setTimeout(() => {
+    //         setLoading(false);
+    //     }, 3000);
+    //     return () => clearTimeout(timer);
+    // }, []);
+    //
+    // if (loading) return null;
+
     return <p>Customers Page</p>
 }
